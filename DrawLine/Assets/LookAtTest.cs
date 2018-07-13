34ea6bd3bee8faad03fe1649296d9dbdec2a786a
ChenWeiChao <1027818327@qq.com>
2018-07-13 21:41:16 +0800

    LookAt与LookRotation的区别

---
+++ DrawLine/Assets/LookAtTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LookAtTest : MonoBehaviour
+{
+
+    public Transform other;//Sphere游戏对象
+
+    void Start()
+    {
+        //Cube的z轴指向（1，1，1）点
+        //        transform.LookAt(new Vector3(1, 1, 1));
+    }
+
+    void Update()
+    {
+        //画线调试，由Cube的postion指向Sphere的postion
+        Debug.DrawLine(transform.position, other.position, Color.cyan);
+        //Cube的z轴指向Sphere，（指向了Sphere游戏对象的Transform组件的position值，也是一个Vector3类型的值） 
+        transform.LookAt(other);
+    }
+}
---
+++ DrawLine/Assets/LookRotationTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class LookRotationTest : MonoBehaviour
+{
+
+    public Transform other;//Sphere游戏对象
+
+    void Start()
+    {
+        //将Cube_1的z轴指向坐标原点（Vector.zero）到 Vector3（2，2，2）所对应的向量方向
+        //transform.rotation = Quaternion.LookRotation(new Vector3(2, 2, 2));
+        //将Cube_1的z轴指向原点到other.position对应向量的方向
+        transform.rotation = Quaternion.LookRotation(other.position);
+    }
+
+    void Update()
+    {
+        //画线调试，画出从Cube_1到Sphere的线段
+        Debug.DrawLine(transform.position, other.position, Color.blue);
+        //画线调试，画出坐标原点到Sphere的射线在cube_1上的表现，即画出向量方向
+        Debug.DrawRay(transform.position, other.position, Color.red);
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");//获取水平偏移量（x轴）
+        float vertical = Input.GetAxis("Vertical");    //获取垂直偏移量（z轴）
+        //将水平偏移量与垂直偏移量组合为一个方向向量
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        //判断是否有水平偏移量与垂直偏移量产生
+        if (direction != Vector3.zero)
+        {
+            //将游戏对象的z轴转向对应的方向向量
+            //            transform.rotation = Quaternion.LookRotation(direction);
+            //对上一行代码进行插值运算则可以将转向表现得较平滑
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction), 0.3f);
+            //将游戏对象进行移动变换方法则可以实现简单的物体移动
+            transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+        }
+    }
+}
