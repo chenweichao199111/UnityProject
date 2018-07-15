a21742e83a06deb88c70c9f8d405673cc82f2cb0
ChenWeiChao <1027818327@qq.com>
2018-07-15 22:22:12 +0800

    程序入口

---
+++ Framework/Assets/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework.UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debuger.Log(NetUtils.SelfIP);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
