e39cd090248ff7c3dced6b783691df7f2f663037
chenweichao <chen15270994267@gmail.com>
2018-07-19 15:56:10 +0800

    优化代码，分析贴图内存

--- Framework/Assets/Framework/Unity/AppStart.cs
+++ Framework/Assets/Framework/Unity/AppStart.cs
@@ -7,10 +7,20 @@ namespace Framework.Unity
         [RuntimeInitializeOnLoadMethod]
         static void Initialize()
         {
-            GameObject tempSingleObj = new GameObject(typeof(AppStart).Name, typeof(AppStart));
-            //mSingleObj.hideFlags = HideFlags.HideInHierarchy;
+            GameObject tempSingleObj = new GameObject(typeof(AppStart).Name);
+            //tempSingleObj.hideFlags = HideFlags.HideInHierarchy;
+            AppStart tempScript = tempSingleObj.AddComponent<AppStart>();
             GameObject.DontDestroyOnLoad(tempSingleObj);
+
 #if UNITY_EDITOR
+            tempScript.OpenLog();
+#else
+            tempScript.CloseLog();
+#endif
+        }
+
+        private void OpenLog()
+        {
             // 如果使用Debuger.Log or Debuger.LogWarnning or Debuger.LogError打印日志, 你可以一键关闭这些日志Hidebug.EnableDebuger(false).
             HiDebug.EnableDebuger(true);
             // 打印日志到屏幕
@@ -18,9 +28,11 @@ namespace Framework.Unity
             // 将会记录日志和堆栈信息到text,默认路径是Application.persistentDataPath
             HiDebug.EnableOnText(true);
             HiDebug.FontSize = 20;
-#else
+        }
+
+        private void CloseLog()
+        {
             HiDebug.EnableDebuger(false);
-#endif
         }
     }
 }
