db456796da8916073ea95544a21d0a4b0aad0280
ChenWeiChao <1027818327@qq.com>
2018-07-28 01:26:01 +0800

    重新上传框架

--- FrameworkLib/Framework/Event/LogicEventType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Framework.Event
-{
-    public partial class LogicEventType
-    {
-        public const uint AppStart = 10000;  // App开始
-        public const uint SwitchAppState = 10001;  // 切换App状态完成
-    }
-}
--- FrameworkLib/Libs/PublishLibrary.bat
+++
@@ -1,5 +0,0 @@
-set CurDir=%~dp0
-set LibName=%1
-pdb2mdb %LibName%.dll
-xcopy %LibName%.dll %CurDir%..\..\Framework\Assets\Plugins\ /y
-xcopy %LibName%.dll.mdb %CurDir%..\..\Framework\Assets\Plugins\ /y
