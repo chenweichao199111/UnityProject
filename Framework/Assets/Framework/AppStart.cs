6e822bd38902f37abdc6a3043973bace03f6bc67
ChenWeiChao <1027818327@qq.com>
2018-08-06 00:26:17 +0800

    上传框架

--- Framework/Assets/Framework/AppStart.cs
+++ Framework/Assets/Framework/AppStart.cs
@@ -22,18 +22,15 @@ namespace Framework.Unity
 
         private void OpenLog()
         {
-            // 如果使用Debuger.Log or Debuger.LogWarnning or Debuger.LogError打印日志, 你可以一键关闭这些日志Hidebug.EnableDebuger(false).
-            HiDebug.EnableDebuger(true);
-            // 打印日志到屏幕
-            HiDebug.EnableOnScreen(true);
-            // 将会记录日志和堆栈信息到text,默认路径是Application.persistentDataPath
-            HiDebug.EnableOnText(true);
-            HiDebug.FontSize = 20;
+            Debuger.EnableLog = true;
+            Debug.unityLogger.logEnabled = true;
+            gameObject.AddComponent<DebugShow>();
         }
 
         private void CloseLog()
         {
-            HiDebug.EnableDebuger(false);
+            Debuger.EnableLog = false;
+            Debug.unityLogger.logEnabled = false;
         }
     }
 }
---
+++ Framework/Assets/Test/TextToSpeech.cs
@@ -0,0 +1,11 @@
+﻿using DotNetSpeech;
+using UnityEngine;
+
+public class TextToSpeech : MonoBehaviour
+{
+    void Start()
+    {
+        SpVoice voice = new SpVoice();
+        voice.Speak("真心换真情");
+    }
+}
